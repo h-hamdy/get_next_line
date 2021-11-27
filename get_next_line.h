1fde6cd24ade3c1f3b6eb5796eeea6ee14ebed2e
Houssam Hamdy <hhamdy@e2r2p9.1337.ma>
2021-11-27 17:02:42 +0100

    work done

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamdy <hhamdy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 15:56:43 by hhamdy            #+#    #+#             */
+/*   Updated: 2021/11/24 22:05:01 by hhamdy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+
+#endif
---
+++ get_next_line_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamdy <hhamdy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 03:14:20 by hhamdy            #+#    #+#             */
+/*   Updated: 2021/11/26 18:09:40 by hhamdy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static int	index_line(const char *s, int c)
+{
+	size_t	i;
+	char	*p;
+
+	i = 0;
+	p = (char *)s;
+	while (p[i])
+	{
+		if (p[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static void	ft_free(void *ptr)
+{
+	free(ptr);
+	ptr = NULL;
+}
+
+static void	get_rest(char **save, char **line, ssize_t end, char **tmp)
+{
+	if (end == 0 && !ft_strchr(*save, '\n'))
+	{
+		*line = ft_strdup(*save);
+		*save = 0;
+	}
+	else
+	{
+		*line = ft_substr(*save, 0, index_line(*save, '\n') + 1);
+		*save = ft_strdup(*save + index_line(*save, '\n') + 1);
+	}
+	ft_free(*tmp);
+	if (**line == '\0')
+	{
+		ft_free(*line);
+		*line = NULL;
+	}
+}
+
+static void	get_line(int fd, char **save, char *buff, char **line)
+{
+	ssize_t	end;
+	char	*tmp;
+
+	end = 1;
+	buff[0] = '\0';
+	while (end != 0 && (!ft_strchr(buff, '\n')))
+	{
+		end = read(fd, buff, BUFFER_SIZE);
+		buff[end] = '\0';
+		tmp = *save;
+		*save = ft_strjoin(tmp, buff);
+		ft_free(tmp);
+	}
+	ft_free(buff);
+	tmp = *save;
+	get_rest(save, line, end, &tmp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*save[OPEN_MAX];
+	char		*buff;
+	char		*line;
+
+	buff = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	if (read(fd, buff, 0) == -1)
+	{
+		ft_free(buff);
+		return (NULL);
+	}
+	if (!save[fd])
+		save[fd] = ft_strdup("");
+	get_line(fd, &save[fd], buff, &line);
+	return (line);
+}
